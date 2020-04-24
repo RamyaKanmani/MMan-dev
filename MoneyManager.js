975a30972812b5e3f502c0777ec570fa2072af52
ramya <ramyakanmanikanmani@gmail.com>
2020-04-24 19:46:35 +0530

    Completed  Submition Button Functionality for Earnings and Savings Section

--- MoneyManager.js
+++ MoneyManager.js
@@ -1,23 +1,26 @@
 document.getElementById("myBtn").addEventListener("click", savingsLIC);
+var licId= 1;
 function savingsLIC() {
-document.querySelector(".demo").innerHTML += "<table class='table table-striped table-responsive-stack'> <tr><td> <label>LIC &nbsp;&nbsp;</label></td><td><input type='text' placeholder='Name'></td><td><input type='number' placeholder='Amount'></input></td><td><select class='currency-selector'><option selected>Select Currency</option> <option>INR</option><option>USD</option><option>EUR</option><option>GBP</option><option>JPY</option> <option>CAD</option><option>AUD</option><option>INR</option></select></td><td><input type='date'></td> </tr></table> ";
+  var str = "<table id="+ "'" + licId + "'" + "class='table table-striped table-responsive-stack'> <tr><td> <label>LIC &nbsp;&nbsp;</label></td><td><input type='text' placeholder='Name'></td><td><input type='number' placeholder='Amount'></input></td><td><select class='currency-selector'><option selected>Select Currency</option> <option>INR</option><option>USD</option><option>EUR</option><option>GBP</option><option>JPY</option> <option>CAD</option><option>AUD</option><option>INR</option></select></td><td><input type='date'></td> </tr></table> ";
+  document.querySelector(".demo").innerHTML += str;
+  licId+=1;
 }
 
 
 document.getElementById("myBtn1").addEventListener("click", savingsFD);
 function savingsFD() {
-document.querySelector(".demo1").innerHTML += "<table class='table table-striped table-responsive-stack'> <tr><td> <label>FD &nbsp;&nbsp;</label></td><td><input type='text' placeholder='Name'></td><td><input type='number' placeholder='Amount'></input></td><td><select class='currency-selector'><option selected>Select Currency</option> <option>INR</option><option>USD</option><option>EUR</option><option>GBP</option><option>JPY</option> <option>CAD</option><option>AUD</option><option>INR</option></select></td><td><input type='date'></td> </tr></table> ";
+  document.querySelector(".demo1").innerHTML += "<table class='table table-striped table-responsive-stack'> <tr><td> <label>FD &nbsp;&nbsp;</label></td><td><input type='text' placeholder='Name'></td><td><input type='number' placeholder='Amount'></input></td><td><select class='currency-selector'><option selected>Select Currency</option> <option>INR</option><option>USD</option><option>EUR</option><option>GBP</option><option>JPY</option> <option>CAD</option><option>AUD</option><option>INR</option></select></td><td><input type='date'></td> </tr></table> ";
 }
 
 
 document.getElementById("myBtn2").addEventListener("click", savingsRD);
 function savingsRD() {
-document.querySelector(".demo2").innerHTML += "<table class='table table-striped table-responsive-stack'> <tr><td> <label>RD &nbsp;&nbsp;</label></td><td><input type='text' placeholder='Name'></td><td><input type='number' placeholder='Amount'></input></td><td><select class='currency-selector'><option selected>Select Currency</option> <option>INR</option><option>USD</option><option>EUR</option><option>GBP</option><option>JPY</option> <option>CAD</option><option>AUD</option><option>INR</option></select></td><td><input type='date'></td> </tr></table> ";
+  document.querySelector(".demo2").innerHTML += "<table class='table table-striped table-responsive-stack'> <tr><td> <label>RD &nbsp;&nbsp;</label></td><td><input type='text' placeholder='Name'></td><td><input type='number' placeholder='Amount'></input></td><td><select class='currency-selector'><option selected>Select Currency</option> <option>INR</option><option>USD</option><option>EUR</option><option>GBP</option><option>JPY</option> <option>CAD</option><option>AUD</option><option>INR</option></select></td><td><input type='date'></td> </tr></table> ";
 }
 
 document.getElementById("myBtn3").addEventListener("click", savingsOthers);
 function savingsOthers() {
-document.querySelector(".demo3").innerHTML += "<table class='table table-striped table-responsive-stack'> <tr><td> <label>Others &nbsp;&nbsp;</label></td><td><input type='text' placeholder='Name'></td><td><input type='number' placeholder='Amount'></input></td><td><select class='currency-selector'><option selected>Select Currency</option> <option>INR</option><option>USD</option><option>EUR</option><option>GBP</option><option>JPY</option> <option>CAD</option><option>AUD</option><option>INR</option></select></td><td><input type='date'></td> </tr></table> ";
+  document.querySelector(".demo3").innerHTML += "<table class='table table-striped table-responsive-stack'> <tr><td> <label>Others &nbsp;&nbsp;</label></td><td><input type='text' placeholder='Name'></td><td><input type='number' placeholder='Amount'></input></td><td><select class='currency-selector'><option selected>Select Currency</option> <option>INR</option><option>USD</option><option>EUR</option><option>GBP</option><option>JPY</option> <option>CAD</option><option>AUD</option><option>INR</option></select></td><td><input type='date'></td> </tr></table> ";
 }
 
 
@@ -66,5 +69,103 @@ function restaurants() {
     
 }
 
+///////////////////////////////////////////////// Displayning Month in Header///////////////////////////////////////////////
 
+function myFunction() {
+  var month = new Array();
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+
+  var d = new Date();
+  var n = month[d.getMonth()];
+  document.getElementById("month").innerHTML = n;
+}
+
+///////////////////////// Fetching Data from Salary and Other Section///////////////////////
+
+function myFunction1() {
+  var x = document.getElementById("salary_id");
+  var result0 = "Your Salary " +x.elements[3].value + " is " +x.elements[1].value + " " +x.elements[2].value ;
+  return result0;
+  
+    
+}
+
+function myFunction2() {
+  var x = document.getElementById("others_id");
+  var result1 = "Your Salary " +x.elements[3].value + " is " +x.elements[1].value + " " +x.elements[2].value ;
+  return result1;
+    
+}
+
+function myFunction3() {
+  var x = document.getElementById("lic_id");
+  var result2 = "Your Investments on LIC is " +x.elements[1].value +x.elements[2].value;
+  return result2;
+    
+}
+
+function myFunction4() {
+  var x = document.getElementById("fd_id");
+  var result3 = "Your Investments on FD is " +x.elements[1].value +x.elements[2].value;
+  return result3;
+    
+}
+function myFunction5() {
+  var x = document.getElementById("rd_id");
+  var result4 = "Your Investments on FD is " +x.elements[1].value +x.elements[2].value;
+  return result4;
+    
+}
+function myFunction6() {
+  var x = document.getElementById("others1_id");
+  var result5 = "Your Investments on FD is " +x.elements[1].value +x.elements[2].value;
+  return result5;
+    
+}
+
+function result()
+{
+    var finalString;   
+    finalString = myFunction1();
+    finalString += "\n";
+    finalString += myFunction2();
+    finalString += "\n";
+    finalString += myFunction3();
+    finalString += "\n";
+    finalString += myFunction4();
+    finalString += "\n";
+    finalString += myFunction5();
+    finalString += "\n";
+    finalString += myFunction6();
+    alert(finalString);
+}
+
+
+
+
+
+
+
+
+
+
+         
+		 
+        
+
+       
+
+        
+    
 
